93f89462508b964ab39b8ba920e33324bc3a239a
ffalt <ffalty@gmail.com>
2017-11-18 19:05:21 +0100

    pre-build script to include version number from package.json

---
+++ build-pre.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const fs = require('fs');
+const appVersion = require('./package.json').version;
+
+console.log('\nRunning pre-build tasks');
+
+const versionFilePath = path.join(__dirname + '/src/environments/version.ts');
+
+const src = `export const version = '${appVersion}';
+`;
+
+// ensure version module pulls value from package.json
+fs.writeFile(versionFilePath, src, { flat: 'w' }, function (err) {
+	if (err) {
+		return console.log(err);
+	}
+	console.log(`Updating application version ${appVersion}`);
+	console.log(`${'Writing version module to '}${versionFilePath}\n`);
+});
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
+import { version } from './version';
 export const environment = {
-	production: true
+	production: true,
+	version: version
 };
