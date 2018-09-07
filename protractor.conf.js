d74debeebe691832fe9d9668296734738210532a
ffalt <ffalty@gmail.com>
2018-09-07 20:44:25 +0200

    update dependencies

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,7 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
+const {jasmine} = require('jasmine');
 const {SpecReporter} = require('jasmine-spec-reporter');
 
 exports.config = {
