f27dc9fff094498020f2663a41600121de88e8e3
ffalt <ffalty@gmail.com>
2018-10-07 14:31:18 +0200

    js linting

--- build-pre.js
+++ build-pre.js
@@ -1,19 +1,18 @@
-const path = require('path');
-const fs = require('fs');
-const appVersion = require('./package.json').version;
+const path = require("path");
+const fs = require("fs");
+const appVersion = require("./package.json").version;
 
-console.log('\nRunning pre-build tasks');
+console.log("\nRunning pre-build tasks");
 
-const versionFilePath = path.join(__dirname + '/src/environments/version.ts');
+const versionFilePath = path.join(__dirname + "/src/environments/version.ts");
 
-const src = `export const version = '${appVersion}';
-`;
+const src = `export const version = "${appVersion}";\n`;
 
 // ensure version module pulls value from package.json
-fs.writeFile(versionFilePath, src, { flat: 'w' }, function (err) {
-	if (err) {
-		return console.log(err);
-	}
-	console.log(`Updating application version ${appVersion}`);
-	console.log(`${'Writing version module to '}${versionFilePath}\n`);
+fs.writeFile(versionFilePath, src, {flat: "w"}, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(`Updating application version ${appVersion}`);
+  console.log(`Writing version module to ${versionFilePath}\n`);
 });
--- protractor.conf.js
+++ protractor.conf.js
@@ -1,29 +1,24 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const {jasmine} = require('jasmine');
-const {SpecReporter} = require('jasmine-spec-reporter');
+const {jasmine} = require("jasmine");
+const {SpecReporter} = require("jasmine-spec-reporter");
 
 exports.config = {
 	allScriptsTimeout: 11000,
-	specs: [
-		'./e2e/**/*.e2e-spec.ts'
-	],
-	capabilities: {
-		'browserName': 'chrome'
-	},
+	specs: ["./e2e/**/*.e2e-spec.ts"],
+	capabilities: {"browserName": "chrome"},
 	directConnect: true,
-	baseUrl: 'http://localhost:4200/',
-	framework: 'jasmine',
+	baseUrl: "http://localhost:4200/",
+	framework: "jasmine",
 	jasmineNodeOpts: {
 		showColors: true,
 		defaultTimeoutInterval: 30000,
-		print: function () {
-		}
+		print() {}
 	},
 	onPrepare() {
-		require('ts-node').register({
-			project: 'e2e/tsconfig.e2e.json'
+		require("ts-node").register({
+			project: "e2e/tsconfig.e2e.json"
 		});
 		jasmine.getEnv().addReporter(new SpecReporter({spec: {displayStacktrace: true}}));
 	}
