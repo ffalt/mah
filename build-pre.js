6ba3d63abe8942649a09774ed46d5386c1f2243b
ffalt <ffalty@gmail.com>
2018-11-18 14:27:25 +0100

    fix(deploy): pre-release script added too much ''''''

--- build-pre.js
+++ build-pre.js
@@ -6,7 +6,7 @@ console.log("\nRunning pre-build tasks");
 
 const versionFilePath = path.join(__dirname + "/src/environments/version.ts");
 
-const src = `export const version = ''${appVersion}'';\n`;
+const src = `export const version = '${appVersion}';\n`;
 
 // ensure version module pulls value from package.json
 fs.writeFile(versionFilePath, src, {flat: "w"}, (err) => {
