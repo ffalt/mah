cb8d5e30a8d076cb23f09199e003becc6526ac13
ffalt <ffalty@gmail.com>
2019-05-15 16:11:38 +0200

    fix(testing): use headless chrome

--- karma.conf.js
+++ karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: false,
-    browsers: ["Chrome"],
+    browsers: ["ChromeHeadless"],
     singleRun: true
   });
 };
