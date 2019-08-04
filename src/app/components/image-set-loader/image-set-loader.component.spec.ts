e8df943ae17fe335587d506e409cefc8aab0ff1a
ffalt <ffalty@gmail.com>
2019-08-04 18:01:35 +0200

    chore(lint): fix

--- src/app/components/image-set-loader/image-set-loader.component.spec.ts
+++ src/app/components/image-set-loader/image-set-loader.component.spec.ts
@@ -1,4 +1,4 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {HttpClientModule} from '@angular/common/http';
 import {SvgdefService} from '../../service/svgdef.service';
