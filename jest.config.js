e004666b94f08589d92c4410faee1bfc3b880c5b
ffalt <ffalty@gmail.com>
2021-11-14 10:21:00 +0100

    build(deps): update to angular 13.0.1

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
-require('jest-preset-angular/ngcc-jest-processor');
-
-/** @type {import('@jest/types').Config.InitialOptions} */
 module.exports = {
 	preset: 'jest-preset-angular',
-	testPathIgnorePatterns: ["/node_modules/", "/local/", "/dist/"],
-	setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+	transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+	setupFilesAfterEnv: ['<rootDir>/setup-jest.ts']
 };
