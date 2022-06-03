9f94e7819f6f777f2b7bc894c9625b6aeedc8502
ffalt <ffalty@gmail.com>
2022-06-03 15:03:00 +0200

    build(android): test android building

---
+++ resources/mobile/android/app/src/main/java/org/ffalt/mah/MainActivity.java
@@ -0,0 +1,5 @@
+package org.ffalt.mah;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ resources/mobile/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'org.ffalt.mah',
+  appName: 'Mah',
+  webDir: '../../dist',
+  bundledWebRuntime: false
+};
+
+export default config;
