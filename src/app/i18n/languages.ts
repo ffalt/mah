868fb31b942794b5c47e5e7f729d6b8dfa2a1387
ffalt <ffalty@gmail.com>
2024-10-31 12:38:21 +0100

    add Euskara (Basque) thanks to https://crowdin.com/profile/mirenbz

--- src/app/i18n/languages.ts
+++ src/app/i18n/languages.ts
@@ -4,6 +4,7 @@ import * as LANG_NL from './nl.json';
 import * as LANG_PT from './pt.json';
 import * as LANG_RU from './ru.json';
 import * as LANG_ES from './es.json';
+import * as LANG_EU from './eu.json';
 
 export const DEFAULT_LANGUAGE = 'en';
 export const LANGUAGES: { [name: string]: { title: string; data: { [key: string]: string } } } = {
@@ -12,7 +13,8 @@ export const LANGUAGES: { [name: string]: { title: string; data: { [key: string]
 	nl: {title: 'Nederlands', data: LANG_NL},
 	pt: {title: 'Português', data: LANG_PT},
 	ru: {title: 'русский', data: LANG_RU},
-	es: {title: 'Español', data: LANG_ES}
+	es: {title: 'Español', data: LANG_ES},
+	eu: {title: 'Euskara', data: LANG_EU}
 };
 
 export const LANGUAGE_TITLES = Object.keys(LANGUAGES).map(key => ({key, title: LANGUAGES[key].title}));
