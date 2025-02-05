528591c78dcfbf8cf20c965ace12d82152d1e1cd
ffalt <ffalty@gmail.com>
2025-02-05 23:26:10 +0100

    update jest-angular

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,4 +1,6 @@
-// eslint-disable-next-line import/no-internal-modules,import/no-unassigned-import
-import 'jest-preset-angular/setup-jest';
+// eslint-disable-next-line import/no-internal-modules
+import { setupZoneTestEnv } from 'jest-preset-angular/setup-env/zone';
 // eslint-disable-next-line import/no-unassigned-import
 import './jest-global-mocks';
+
+setupZoneTestEnv();
