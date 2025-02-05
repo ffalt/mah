2eeab8bacde04c537c59d64a6e1d367ab47716d7
ffalt <ffalty@gmail.com>
2025-02-05 18:59:06 +0100

    add languages fr & jp

--- src/app/i18n/languages.ts
+++ src/app/i18n/languages.ts
@@ -5,6 +5,8 @@ import * as LANG_PT from './pt.json';
 import * as LANG_RU from './ru.json';
 import * as LANG_ES from './es.json';
 import * as LANG_EU from './eu.json';
+import * as LANG_JP from './jp.json';
+import * as LANG_FR from './fr.json';
 
 export const DEFAULT_LANGUAGE = 'en';
 export const LANGUAGES: { [name: string]: { title: string; data: { [key: string]: string } } } = {
@@ -14,7 +16,9 @@ export const LANGUAGES: { [name: string]: { title: string; data: { [key: string]
 	pt: {title: 'Português', data: LANG_PT},
 	ru: {title: 'русский', data: LANG_RU},
 	es: {title: 'Español', data: LANG_ES},
-	eu: {title: 'Euskara', data: LANG_EU}
+	eu: {title: 'Euskara', data: LANG_EU},
+	jp: {title: '日本語', data: LANG_JP},
+	fr: {title: 'Français', data: LANG_FR}
 };
 
 export const LANGUAGE_TITLES = Object.keys(LANGUAGES).map(key => ({key, title: LANGUAGES[key].title}));
