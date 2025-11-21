0ac7ad45f6fa3c5bb82c6a944439be2ad4289e0f
ffalt <ffalty@gmail.com>
2025-11-21 19:17:25 +0100

    chore(deps): update

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { enableProdMode, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
@@ -20,7 +20,7 @@ if (environment.production) {
 
 bootstrapApplication(AppComponent, {
 	providers: [
-		importProvidersFrom(
+		provideZoneChangeDetection(),importProvidersFrom(
 			BrowserModule,
 			CommonModule,
 			...environment.modules
